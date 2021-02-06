20d14456689b12f4bded634dda6483ad5cada8dd
Alexander Pimenov <pimalex1978@yandex.ru>
2021-02-06 21:01:59 +0300

    2. Scriplet. Динамическая таблица.[276520#301878]

---
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -0,0 +1,29 @@
+package ru.job4j.dream.store;
+
+import ru.job4j.dream.model.Post;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/*
+ * Класс Store - хранилище вакансию.
+ */
+public class Store {
+    private static final Store INST = new Store();
+    private Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    private Store() {
+        posts.put(1, new Post(1, "Junior Java Job", "Some description"));
+        posts.put(2, new Post(2, "Middle Java Job", "Some description"));
+        posts.put(3, new Post(3, "Senior Java Job", "Some description"));
+    }
+
+    public static Store instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+}
